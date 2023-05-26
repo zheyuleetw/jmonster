da873d620bdbc5fa37e3d87d200a3a411d414cf7
Ken Lee <ken1019096@gmail.com>
2023-05-26 18:27:02 +0800

    :bug: added component scan

--- web/src/main/kotlin/org/jmonster/codegenerator/web/JmonsterWebApplication.kt
+++ web/src/main/kotlin/org/jmonster/codegenerator/web/JmonsterWebApplication.kt
@@ -2,10 +2,12 @@ package org.jmonster.codegenerator.web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(value = ["org.jmonster.codegenerator.common", "org.jmonster.codegenerator.web"])
 class JmonsterApplication
 
 fun main(args: Array<String>) {
-	runApplication<JmonsterApplication>(*args)
+    runApplication<JmonsterApplication>(*args)
 }
